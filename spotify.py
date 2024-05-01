0b47e94735702d9141cc0aca1e40a157a4e774cb
Walke <92731842+s4ndisk@users.noreply.github.com>
2024-05-01 16:52:40 +0000

    initial (needs testing)

--- spotify.py
+++ spotify.py
@@ -19,17 +19,22 @@ album_appeared_on = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='appear
 async def latest_album():
     album_id = album['id']
     album_url = album['external_urls']['spotify']
+    return album_url
 
 async def latest_single():
     single_id = single['id']
     single_url = single['external_urls']['spotify']
+    return single_url
 
 async def latest_album_appeared_on():
-   album_appeared_on_id = album_appeared_on['id']
-   album_appeared_on_url = album_appeared_on['external_urls']['spotify']
-                       
+    album_appeared_on_id = album_appeared_on['id']
+    album_appeared_on_url = album_appeared_on['external_urls']['spotify']
+    return album_appeared_on_url   
+          
 async def main():
-    await asyncio.gather(latest_album(), latest_single(), latest_album_appeared_on())
-
+    while True:
+        await asyncio.gather(latest_album(), latest_single(), latest_album_appeared_on())
+        asyncio.sleep(300)
+        
 if __name__ == "__main__":
     asyncio.run(main())
