c5931f4f2fa3e9a5861f6d467f9fb0c891f53989
Walke <92731842+s4ndisk@users.noreply.github.com>
2024-05-01 10:27:20 -0400

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -12,13 +12,24 @@ SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 spotipy = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-latest_album = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='album', limit=1)
-latest_single = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='single', limit=1)
-latest_album_appeared_in = spotipy.artist_albumes(SPOTIFY_ARTIST_URI, album_type='appears_on', limit=1)
+album = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='album', limit=1)
+single = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='single', limit=1)
+album_appeared_on = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='appears_on', limit=1)
 
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
+async def latest_album:
+    album_id = album['id']
+    album_url = album['external_urls']['spotify']
 
+async def latest_single:
+    single_id = single['id']
+    album_url = album['external_urls']['spotify']
+
+async def latest_album_appeared_on:
+   album_appeared_on_id = album_appeared_on['id']
+   album_appeared_on_url = album_appeared_on['external_urls']['spotify']
+                       
+async def main():
+    await asyncio.gather(latest_album(), latest_single(), latest_album_appeared_on())
+
+if __name__ == "__main__":
+    asyncio.run(main())
