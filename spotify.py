03e417c7d32278995bea55a5462e39b1634cd3c1
s4ndisk <walker.hopkins@proton.me>
2024-05-01 17:29:20 -0400

    working

--- spotify.py
+++ spotify.py
@@ -1,7 +1,8 @@
+import os
 import asyncio
 import spotipy 
-import os
 import json
+from datetime import datetime
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
@@ -11,26 +12,37 @@ SPOTIFY_ARTIST_URI = os.getenv("SPOTIFY_ARTIST_URI")
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-spotipy = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
 
-album = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='album', limit=1)
-single = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='single', limit=1)
-album_appeared_on = spotipy.artist_albums(SPOTIFY_ARTIST_URI, album_type='appears_on', limit=1)
+album = sp.artist_albums(SPOTIFY_ARTIST_URI, album_type='album', limit=1)
+single = sp.artist_albums(SPOTIFY_ARTIST_URI, album_type='single', limit=1)
+album_appeared_on = sp.artist_albums(SPOTIFY_ARTIST_URI, album_type='appears_on', limit=1)
 
 POLLING_INTERVAL = 60 * 5
 
-def search_spotify_data(s_id):
+async def get_latest(data):
+    if data is album:
+        global latest_album_url
+        return latest_album_url
+    elif data is single:
+        global latest_single_url
+        return latest_single_url
+    elif data is album_appeared_on:
+        global latest_album_appeared_on
+        return latest_album_appeared_on
+
+def search_spotify_data(url):
     with open('ids.json', 'r') as file:
         data = json.load(file)
     
     for spotify_data in data['spotify']:
-        spotify_id = spotify_data['spotify_id']
-        if spotify_id in s_id:
+        spotify_url = spotify_data['spotify_url']
+        if spotify_url in url:
             return True
     return False
 
-def append_spotify_data(s_id, url):
-    new_data = {"spotify_ids": s_id, "spotify_urls": url}
+def append_spotify_data(url, date):
+    new_data = {"spotify_url": url, "spotify_date": date}
     with open('ids.json', 'r+') as file:
         data = json.load(file)
         data["spotify"].append(new_data)
@@ -38,36 +50,63 @@ def append_spotify_data(s_id, url):
         json.dump(data, file, indent=4)
 
 async def latest_album():
+    global latest_album_url
+    latest_album_url = None
     while True:
-        album_id = album['id']
-        album_url = album['external_urls']['spotify']
-        if album_id:
-            for s_id in album_id:
-                if not search_spotify_data([album_id]):
-                    append_spotify_data(album_id, album_url)
-                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} New album found: {album_id}")
+        album_url = album['items'][0]['external_urls']['spotify']
+        album_date = album['items'][0]['release_date']
+        if album_url:
+            for url in album_url:
+                if not search_spotify_data([album_url]):
+                    append_spotify_data(album_url, album_date)
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} New album found: {album_url}")
                     latest_album_url = album_url
-                    return latest_album_url
-        if latest_album_id == None:
+        if latest_album_url == None:
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} No new albums found")
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Sleeping for {POLLING_INTERVAL}s")
         await asyncio.sleep(POLLING_INTERVAL)
         
 
 async def latest_single():
-    single_id = single['id']
-    single_url = single['external_urls']['spotify']
-    return single_url
+    global latest_single_url
+    latest_single_url = None
+    while True:
+        single_url = single['items'][0]['external_urls']['spotify']
+        single_date = single['items'][0]['release_date']
+        if single_url:
+            for url in single_url:
+                if not search_spotify_data([single_url]):
+                    append_spotify_data(single_url, single_date)
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} New single found: {single_url}")
+                    latest_single_url = single_url
+        if latest_single_url == None:
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} No new singles found")
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Sleeping for {POLLING_INTERVAL}s")
+        await asyncio.sleep(POLLING_INTERVAL)
+   
 
 async def latest_album_appeared_on():
-    album_appeared_on_id = album_appeared_on['id']
-    album_appeared_on_url = album_appeared_on['external_urls']['spotify']
-    return album_appeared_on_url   
+    global latest_album_appeared_on
+    latest_album_appeared_on = None
+    while True:
+        album_appeared_on_url = album_appeared_on['items'][0]['external_urls']['spotify']
+        album_appeared_on_date = album_appeared_on['items'][0]['release_date']
+        if album_appeared_on_url:
+            for url in album_appeared_on_url:
+                if not search_spotify_data([album_appeared_on_url]):
+                    append_spotify_data(album_appeared_on_url, album_appeared_on_date)
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} New album appeared on found: {album_appeared_on_url}")
+                    latest_album_appeared_on_url = album_appeared_on_url
+        if latest_album_appeared_on_url == None:
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} No new albums appeared on found")
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Sleeping for {POLLING_INTERVAL}s")
+        await asyncio.sleep(POLLING_INTERVAL)
+    
           
 async def main():
     while True:
         await asyncio.gather(latest_album(), latest_single(), latest_album_appeared_on())
-        asyncio.sleep(300)
+        await asyncio.sleep(300)
 
 if __name__ == "__main__":
     asyncio.run(main())
